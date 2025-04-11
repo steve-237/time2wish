9cd898cbe42144b13aa37106445c60e5c5229c1a
steve-237 <steve0peg@gmail.com>
2025-04-11 23:23:22 +0200

    feat: add reset password page

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,18 +3,14 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegistrationComponent } from './registration/registration.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 export const routes: Routes = [
   { path: 'landing-page', component: LandingPageComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent},
+  { path: 'register', component: RegistrationComponent },
+  { path: 'password-reset', component: PasswordResetComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  {
-    path: 'page-not-found',
-    component: PageNotFoundComponent,
-  },
-  {
-    path: '**',
-    redirectTo: '/page-not-found',
-  },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/page-not-found' },
 ];
