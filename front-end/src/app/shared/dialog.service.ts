d17a4297e7fda7b8c78d32a0c9e2ad91f1928282
steve-237 <steve0peg@gmail.com>
2025-04-24 15:08:32 +0200

    feat: add modal

---
+++ front-end/src/app/shared/dialog.service.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable, Type } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+
+  private dialog = inject(MatDialog);
+
+  open(component: Type<any>, config: { width?: string; data?: any } = {}) {
+    return this.dialog.open(component, {
+      width: config.width || '400px',
+      data: config.data || null,
+      disableClose: false,
+      autoFocus: true,
+    });
+  }
+}
