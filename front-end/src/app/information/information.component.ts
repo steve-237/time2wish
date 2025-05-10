20df883f93e05efa33ab8b1abf22ab915f4c9574
steve-237 <steve0peg@gmail.com>
2025-05-10 21:35:52 +0200

    feat: add information page

---
+++ front-end/src/app/information/information.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+
+@Component({
+  selector: 'app-information',
+  imports: [MatCardModule, MatIconModule, MatExpansionModule, MatListModule],
+  templateUrl: './information.component.html',
+  styleUrl: './information.component.css',
+})
+export class InformationComponent {}
