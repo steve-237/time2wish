19b0355e731d646cf4c9bf6aba3f629232e58b8a
steve-237 <steve0peg@gmail.com>
2025-05-29 23:16:25 +0200

    feat: display birthday details

---
+++ front-end/src/app/pages/birthday-details/birthday-details.component.ts
@@ -0,0 +1,101 @@
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+} from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-birthday-details',
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatCardModule,
+  ],
+  templateUrl: './birthday-details.component.html',
+  styleUrl: './birthday-details.component.css',
+})
+export class BirthdayDetailsComponent {
+  constructor(
+    public dialogRef: MatDialogRef<BirthdayDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router
+  ) {}
+
+  /**
+   * Détermine si l'anniversaire est passé
+   */
+  isBirthdayPassed(birthdate: Date): boolean {
+    const today = new Date();
+    const thisYearBirthday = new Date(birthdate);
+    thisYearBirthday.setFullYear(today.getFullYear());
+
+    return thisYearBirthday < today;
+  }
+
+  /**
+   * Retourne l'icône correspondant au statut
+   */
+  getStatusIcon(date: Date): string {
+    return this.isBirthdayPassed(date) ? 'event_busy' : 'event_available';
+  }
+
+  /**
+   * Retourne le texte correspondant au statut
+   */
+  getStatusText(date: Date): string {
+    return this.isBirthdayPassed(date) ? 'Passé' : 'À venir';
+  }
+
+  /**
+   * Calcul de l'âge
+   */
+  calculateAge(birthdate: Date): number {
+    const today = new Date();
+    const birthDate = new Date(birthdate);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
+      age--;
+    }
+
+    return age;
+  }
+
+  getCategoryIcon(category: string): string {
+    switch (category.toLowerCase()) {
+      case 'family':
+        return 'family_restroom';
+      case 'friend':
+        return 'group';
+      case 'colleague':
+        return 'work';
+      case 'business':
+        return 'business_center';
+      default:
+        return 'category';
+    }
+  }
+
+  /**
+   * Gestion de l'édition
+   */
+  onEdit(): void {}
+}
