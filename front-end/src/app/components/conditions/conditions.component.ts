273862c8e7e5f03e676ee15ac72ac3428c10baca
steve-237 <steve0peg@gmail.com>
2025-10-23 16:54:48 +0200

    add component for the footer

---
+++ front-end/src/app/components/conditions/conditions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conditions',
+  imports: [],
+  templateUrl: './conditions.component.html',
+  styleUrl: './conditions.component.css'
+})
+export class ConditionsComponent {
+
+}
--- front-end/src/app/components/footer/footer.component.ts
+++ front-end/src/app/components/footer/footer.component.ts
@@ -2,13 +2,43 @@ import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslocoModule } from '@jsverse/transloco';
+import { DialogService } from '../../shared/services/dialog/dialog.service';
+import { HelpsComponent } from '../helps/helps.component';
+import { ConditionsComponent } from '../conditions/conditions.component';
+import { ConfidentialityComponent } from '../confidentiality/confidentiality.component';
 
 @Component({
   selector: 'app-footer',
   imports: [MatIconModule, TranslocoModule, DatePipe],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
+
 export class FooterComponent {
   currentDate: Date = new Date();
+
+  private modalComponentMap = {
+    help: HelpsComponent,
+    conditions: ConditionsComponent,
+    confidentiality: ConfidentialityComponent,
+  };
+
+  constructor(private dialog: DialogService) {}
+
+  /**
+   * Opens a modal dialog displaying the specified component.
+   *
+   * @param component - The type of modal to open. Can be 'help', 'conditions', or 'confidentiality'.
+   * 
+   * Looks up the corresponding component class from `modalComponentMap` and opens it in a dialog
+   * with a fixed width of 500px using the Angular Material Dialog service.
+   */
+  public openModal(component: 'help' | 'conditions' | 'confidentiality') {
+    const componentClass = this.modalComponentMap[component];
+    if (componentClass) {
+      this.dialog.open(componentClass, {
+        width: '500px',
+      });
+    }
+  }
 }
