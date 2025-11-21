084d2c9b8bbe95d052f5adceed9621223fc930d3
steve-237 <steve0peg@gmail.com>
2025-11-21 01:14:50 +0100

    added guard for auth

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -4,13 +4,14 @@ import { LoginComponent } from './pages/login/login.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { PasswordResetComponent } from './pages/password-reset/password-reset.component';
+import { AuthGuard } from './core/guard/auth/auth.guard';
 
 export const routes: Routes = [
-  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'landing-page', component: LandingPageComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: 'password-reset', component: PasswordResetComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'page-not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: '/page-not-found' },
+  { path: 'page-not-found', component: PageNotFoundComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '/login' },
 ];
