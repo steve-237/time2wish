2d0c33295de9a98de241d78ec8c3d8728657d706
steve-237 <steve0peg@gmail.com>
2026-02-14 16:56:35 +0100

    add lazy loading

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,18 +1,31 @@
 import { Routes } from '@angular/router';
-import { LandingPageComponent } from './pages/landing-page/landing-page.component';
-import { LoginComponent } from './pages/login/login.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { RegistrationComponent } from './pages/registration/registration.component';
-import { PasswordResetComponent } from './pages/password-reset/password-reset.component';
 import { AuthGuard } from './core/guard/auth/auth.guard';
-import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegistrationComponent },
-  { path: 'password-reset', component: PasswordResetComponent },
-  { path: 'reset-password', component: ResetPasswordComponent },
-  { path: 'page-not-found', component: PageNotFoundComponent},
+  { 
+    path: '', 
+    loadComponent: () => import('./pages/landing-page/landing-page.component').then(m => m.LandingPageComponent),
+    canActivate: [AuthGuard] 
+  },
+  { 
+    path: 'login', 
+    loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) 
+  },
+  { 
+    path: 'register', 
+    loadComponent: () => import('./pages/registration/registration.component').then(m => m.RegistrationComponent) 
+  },
+  { 
+    path: 'password-reset', 
+    loadComponent: () => import('./pages/password-reset/password-reset.component').then(m => m.PasswordResetComponent) 
+  },
+  { 
+    path: 'reset-password', 
+    loadComponent: () => import('./pages/reset-password/reset-password.component').then(m => m.ResetPasswordComponent) 
+  },
+  { 
+    path: 'page-not-found', 
+    loadComponent: () => import('./pages/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+  },
   { path: '**', redirectTo: '' },
 ];
